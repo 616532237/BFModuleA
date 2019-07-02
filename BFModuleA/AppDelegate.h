397044d566fd75f972166da63a522a9e4feb9ff9
qifeibo <616532237@qq.com>
2019-07-02 18:01:37 +0800

    Initial Commit

---
+++ BFModuleA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFModuleA
+//
+//  Created by 齐飞薄 on 2019/7/2.
+//  Copyright © 2019 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFModuleA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFModuleA
+//
+//  Created by 齐飞薄 on 2019/7/2.
+//  Copyright © 2019 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
